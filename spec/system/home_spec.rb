a113eab962c0ff23e98d95d17b4e0b12bb854340
taka498 <y.ktcae5e4d3@gmail.com>
2025-10-10 16:18:11 +0900

    学習ログ等高機能を作成

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
  
 RSpec.describe 'Home', type: :system do
   before do
-    driven_by :rack_test
+    driven_by(:rack_test)
   end
  
-  describe 'トップページアクセスの検証' do
-    it 'Home#top という文字列が表示される' do
+  describe 'トップページの検証' do
+    it 'Home#topという文字列が表示される' do
       visit '/'
  
       expect(page).to have_content('Home#top')
     end
   end
  
-  ####### ここから追加 #######
   describe 'ナビゲーションバーの検証' do
     context 'ログインしていない場合' do
       before { visit '/' }
@@ -24,6 +23,10 @@ RSpec.describe 'Home', type: :system do
  
       it 'ログインリンクを表示する' do
         expect(page).to have_link('ログイン', href: '/users/sign_in')
+      end
+ 
+      it 'ログ投稿リンクを表示しない' do # 追加
+        expect(page).not_to have_link('ログ投稿', href: '/posts/new')
       end
  
       it 'ログアウトリンクは表示しない' do
@@ -46,6 +49,10 @@ RSpec.describe 'Home', type: :system do
         expect(page).not_to have_link('ログイン', href: '/users/sign_in')
       end
  
+      it 'ログ投稿リンクを表示する' do # 追加
+        expect(page).to have_link('ログ投稿', href: '/posts/new')
+      end
+ 
       it 'ログアウトリンクを表示する' do
         expect(page).to have_content('ログアウト')
       end
@@ -61,3 +68,4 @@ RSpec.describe 'Home', type: :system do
     end
   end
 end
+
